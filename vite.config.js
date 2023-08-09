02cd80be00e752a8a87cc0caaf741bd489962485
deep0133 <deepanshuheer27@gmail.com>
2023-08-09 13:00:49 +0530

    changed server host:true

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // un-comment while working in local
-  // server: {
-  //   host: "localhost",  // ip address not store cookies in browser
-  //   port: "3002",
-  // },
+  server: {
+    host: true,
+    // port: 3002,
+  },
 })
 
